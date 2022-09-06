06a7e3514bf6e0cfee8179de58897d97ddac117b
gustarmp <gustavo360@protonmail.com>
2022-09-05 21:22:04 -0300

    Add Components

--- App-ioLink/src/app/home/home.page.ts
+++ App-ioLink/src/app/home/home.page.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface IPessoa {
+  nome: string;
+  sobrenome: string;
+}
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +13,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public texto = 'Boa noite pessoal.'
 
+  public pessoa: IPessoa = {
+    nome: 'Gustavo',
+    sobrenome: 'Martins'
+  };
+
+  public cor = 'blue'
+  
+  public currentvalue = ''
+
+  public switch = true;
+
+  public valorNumerico = 0;
+
+  aumentar(): void {
+    if (this.valorNumerico < 10) this.valorNumerico++;
+  }
+
+  diminuir(): void {
+    if (this.valorNumerico > 0) this.valorNumerico--;
+  }
+ 
+  resetar(): void{
+    this.valorNumerico = 0;
+  }
+  
 }
